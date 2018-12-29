5646b6659e5f84f6ab88954adef3f5628d010c62
Dan Kiss <flowfast47@gmail.com>
2018-12-28 17:39:53 -0800

    Updates

---
+++ precache-manifest.5583268c1d5c4f7ca5c63f5afd52f3ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crypto-tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a9cc849a067d36f5a51",
+    "url": "/crypto-tracker/static/js/runtime~main.8a9cc849.js"
+  },
+  {
+    "revision": "ace5cbee7776eb5483ce",
+    "url": "/crypto-tracker/static/js/main.ace5cbee.chunk.js"
+  },
+  {
+    "revision": "e0fd798fd7b880664dac",
+    "url": "/crypto-tracker/static/js/1.e0fd798f.chunk.js"
+  },
+  {
+    "revision": "ace5cbee7776eb5483ce",
+    "url": "/crypto-tracker/static/css/main.49b9d132.chunk.css"
+  },
+  {
+    "revision": "5cd12af5b960ea4e0b81592121522698",
+    "url": "/crypto-tracker/index.html"
+  }
+];
