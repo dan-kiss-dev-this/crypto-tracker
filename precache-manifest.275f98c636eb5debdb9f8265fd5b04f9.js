4bb01f9b43ecca5fcf7378b8c3c9ff8476958451
Dan Kiss <flowfast47@gmail.com>
2018-12-31 21:51:25 -0800

    Updates

--- precache-manifest.275f98c636eb5debdb9f8265fd5b04f9.js
+++ precache-manifest.275f98c636eb5debdb9f8265fd5b04f9.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/crypto-tracker/static/js/runtime~main.8a9cc849.js"
   },
   {
-    "revision": "b16ea5d19771c7857aae",
-    "url": "/crypto-tracker/static/js/main.b16ea5d1.chunk.js"
+    "revision": "683e34fe4672ae3e94f0",
+    "url": "/crypto-tracker/static/js/main.683e34fe.chunk.js"
   },
   {
     "revision": "ad85a32d628465d59bc9",
     "url": "/crypto-tracker/static/js/1.ad85a32d.chunk.js"
   },
   {
-    "revision": "b16ea5d19771c7857aae",
+    "revision": "683e34fe4672ae3e94f0",
     "url": "/crypto-tracker/static/css/main.acbb7bfe.chunk.css"
   },
   {
-    "revision": "dc154c428c6b5b47eca055f4c449f504",
+    "revision": "51740b776419bda043295c1b062eee4b",
     "url": "/crypto-tracker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crypto-tracker/precache-manifest.5b2cd5dc242d1e6d84550e7f06d1b15f.js"
+  "/crypto-tracker/precache-manifest.275f98c636eb5debdb9f8265fd5b04f9.js"
 );
 
 workbox.clientsClaim();
